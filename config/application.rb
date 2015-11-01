1c34fd6c33cd7c3fd95782c1996ebb2b7802d601
harry13 <xterminator_hr13@icloud.com>
2015-11-01 11:48:43 +0000

    add followings/followers

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,10 @@ module Microposts
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    #Ajaxでリクエストを送信する際に、トークンという文字列を返す為の設定
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+
     #テストコードとJavaScript、CSS(SCSS)、ヘルパのファイルを自動生成しない
     config.generators do |g|
       g.stylesheets false
